a454e317b071ade5f8d4a88ab199d43133c43168
jennyfer04ramirez <jr.6311.camm@gmail.com>
2025-01-29 08:26:26 -0500

    cmabios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CandidatoServiceService } from './services/candidato-service.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,24 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'evaluacionRamirezAngular';
+  id = "";
+  candidato: any[] = [];
+  binomio:any[] = [];
+
+  constructor(private service: CandidatoServiceService) {}
+
+  ngOnInit(): void {}
+
+  buscarPorId() {
+    this.service.getCandidato(this.id).subscribe((data) => {
+      (data:any) => {
+      this.candidato = data.candidato;
+      if(this.candidato.length > 0){
+        //this.binomio = this.service.calcularBinomio(this.candidato[0].puntaje);
+      } else {
+        this.binomio = [];
+      }
+      }
+    });
+  }
 }
